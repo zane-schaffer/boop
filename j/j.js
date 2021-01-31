2aaa96a6c70ac875e3f221dca2bddb95b19b9938
Zane Schaffer <hello@zanes.house>
2021-01-31 14:45:26 -0800

    color darken

---
+++ j/j.js
@@ -0,0 +1,10 @@
+newFunction();
+function newFunction() {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  const clock = document.querySelector(".time");
+  clock.innerHTML = `time : '<a>${hours}:${minutes}</a>',`;
+}
+
+setInterval(newFunction, 1000)
