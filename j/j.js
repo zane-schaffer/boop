986cf3301a1432c7e4d7752a0e680e0307b4e5ab
Zane Schaffer <hello@zanes.house>
2021-01-31 22:17:00 -0800

    font updated yayyyyy

--- j/j.js
+++ j/j.js
@@ -2,10 +2,16 @@ newFunction();
 function newFunction() {
   let time = new Date(new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" }));
   let hours = time.getHours();
+
   let minutes = time.getMinutes();
+
   minutes < 10 ? minutes = `0${minutes}` : minutes
+  hours < 10 ? hours = `0${hours}` : hours
   const clock = document.querySelector(".time");
   clock.innerHTML = `time : '<a>${hours}:${minutes}</a>',`;
 }
+
+
+
 
 setInterval(newFunction, 1000)
